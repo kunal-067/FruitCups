b7b1831331cca8fa05448ac10ce1841cfd1b5370
Kunal Shroff <kk9122874046@gmail.com>
2025-09-17 21:02:08 +0530

    Add files via upload

---
+++ src/app/ToastWrapper.jsx
@@ -0,0 +1,18 @@
+'use client'
+// import dynamic from 'next/dynamic';
+import React from 'react';
+import { Toaster } from '@/components/ui/sonner';
+import ReduxProviders from './ReduxProvider';
+
+// const ReduxProviders = dynamic(() => import('./ReduxProvider.jsx'), {
+//   ssr: false, // Disable server-side rendering for this component
+// });
+
+export default function Wrapper({ children }) {
+  return (
+    <>
+      <ReduxProviders>{children}</ReduxProviders>
+      <Toaster />
+    </>
+  );
+}
