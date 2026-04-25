26a3de4058a67892907fa62380c3d7a24f2a6e18
itz-kunal <kunal.wd067@gmail.com>
2026-04-25 21:15:34 +0530

    db issue

--- src/utils/dbconnect.js
+++ src/utils/dbconnect.js
@@ -1,38 +1,27 @@
 import mongoose from "mongoose";
-const MONGODB_URI = process.env.MONGODB_URI ;
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    throw new Error("Please define MONGODB_URI");
 }
 
-// Global cache for connection
-let cachedCon = global.mongoose || {
-    conn: null
-};
+let cached = global.mongoose || { conn: null, promise: null };
 
 if (!global.mongoose) {
-    global.mongoose = cachedCon;
+    global.mongoose = cached;
 }
 
 export async function connectDb() {
-    try {
-        // console.log(cachedCon.conn, 'cachedCon conn')
-        if (cachedCon.conn) {
-            return cachedCon.conn;
-        }
+    if (cached.conn) return cached.conn;
 
-        const opts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            authSource: "admin",
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
             bufferCommands: false,
-            dbName: 'fruitcup',
-        };
-
-        cachedCon.conn = await mongoose.connect(MONGODB_URI, opts);
-        console.log('connected to db - ', MONGODB_URI)
-        return cachedCon.conn;
-    } catch (err) {
-        console.error('Error in connecting db', err)
+            dbName: "fruitcup",
+        }).then((mongoose) => mongoose);
     }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
